baa868d32e1f6dedbeba41ab615a8afb2983cd8c
nelacopot <nela.copot@student.um.si>
2025-05-24 23:48:03 +0200

    SCRUM-51 Dodan Docker in Dockerfiles v spletno aplikacijo

--- web/backend/app.js
+++ web/backend/app.js
@@ -11,19 +11,8 @@ const app = express();
 
 const allowedOrigins = ['http://localhost:3000', 'http://127.0.0.1:3000'];
 app.use(cors({
-  credentials: true,
-  origin: function(origin, callback){
-    if (!origin) return callback(null, true);
-    if (
-      origin.startsWith('http://localhost:') ||
-      origin.startsWith('http://127.0.0.1:') ||
-      /^http:\/\/172\.\d+\.\d+\.\d+:\d+$/.test(origin) // Allow Docker network IPs
-    ) {
-      return callback(null, true);
-    }
-    var msg = "The CORS policy does not allow access from the specified Origin.";
-    return callback(new Error(msg), false);
-  }
+  origin: '*', // samo za razvoj
+  credentials: true
 }));
 
 app.use(express.json({ limit: '100mb' })); // Limit request size to 100MB
